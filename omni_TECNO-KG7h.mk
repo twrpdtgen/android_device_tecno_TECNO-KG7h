e69206327da48e7749e102650e932640015fd5ee
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-24 08:11:51 +0200

    TECNO-KG7h: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO KG7h Script version: 2.4.0

---
+++ omni_TECNO-KG7h.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from TECNO-KG7h device
+$(call inherit-product, device/tecno/TECNO-KG7h/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := TECNO-KG7h
+PRODUCT_NAME := omni_TECNO-KG7h
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO KG7h
+PRODUCT_MANUFACTURER := tecno
